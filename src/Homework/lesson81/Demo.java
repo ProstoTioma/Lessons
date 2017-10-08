6f0ea12c4f68f23499b09b5eadb64cbf69bd64dc
ProstoTioma <artemyermolenko14@gmail.com>
2017-10-08 18:00:56 +0200

    Arithmetic Task

--- src/Homework/lesson81/Demo.java
+++ src/Homework/lesson81/Demo.java
@@ -2,12 +2,12 @@ package Homework.lesson81;
 
 public class Demo {
     public Student createHighestParent() {
-        Student student = new Student("Artem", "Yermolenko", 10, null);
+        Student student = new Student("Artem", "Yermolenko", 10, new Course[]{} );
         return student;
     }
 
     public SpecialStudent createLowestChild() {
-        SpecialStudent specialStudent = new SpecialStudent("WAT", "WAR", 10, null, 1111, "Boy");
+        SpecialStudent specialStudent = new SpecialStudent("WAT", "WAR", 10, new Course[]{}, 1111, "Boy");
         return specialStudent;
     }
 }
