ea5e2be722967176b6748da388f587365de0c6c6
ProstoTioma <artemyermolenko14@gmail.com>
2017-10-08 17:00:09 +0200

    JAK

---
+++ src/lesson8/accounts/CheckingAccount.java
@@ -0,0 +1,16 @@
+package lesson8.accounts;
+
+public class CheckingAccount extends Account {
+    int limitOFExpenses;
+
+    public CheckingAccount(String bankName, String ownerName, int moneyAmount, int limitOFExpences) {
+        super(bankName, ownerName, moneyAmount);
+        this.limitOFExpenses = limitOFExpences;
+    }
+
+    void withdraw(int amount) {
+        if(amount > limitOFExpenses)
+            return;
+        moneyAmount -= amount;
+    }
+}
