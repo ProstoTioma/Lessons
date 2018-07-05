19362b6acd69a4584657bb87b0480fdc9eb50e3b
JAVA <artemyermolenko14@gmail.com>
2018-07-05 11:15:40 +0200

    Java

--- src/Homework/lesson11/BookingComAPI.java
+++ src/Homework/lesson11/BookingComAPI.java
@@ -3,6 +3,7 @@ package Homework.lesson11;
 public class BookingComAPI implements API {
     private Room[] rooms;
     private Room[] rooms1;
+    private Room room;
 
 
     public BookingComAPI(Room[] rooms) {
@@ -16,8 +17,14 @@ public class BookingComAPI implements API {
 
         for (Room room : rooms) {
             if (price >= price - 100 && price <= price + 100) {
-                System.out.println(price + " " + persons + " " + city + " " + hotel);
-                rooms1 = rooms;
+                if(persons == room.getPersons()) {
+                    if(city == room.getCityName()) {
+                        if(hotel == room.getHotelName()) {
+                            System.out.println(price + " " + persons + " " + city + " " + hotel);
+                            rooms1 = rooms;
+                        }
+                    }
+                }
             }
         }
         return rooms1;
