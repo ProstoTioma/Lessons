39397f9facb158ddbed3e11cd2b8830a623ed1d3
JAVA <artemyermolenko14@gmail.com>
2018-07-07 14:13:45 +0200

    Java

--- src/Homework/lesson11/TripAdvisorAPI.java
+++ src/Homework/lesson11/TripAdvisorAPI.java
@@ -10,18 +10,37 @@ public class TripAdvisorAPI implements API{
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        Room[] rooms1;
+        Room[] array = new Room[1];
         for (Room room : rooms) {
-            if (persons >= persons - 1 && persons <= persons + 1) {
-                if(persons == room.getPersons())
+            if (room.getPersons() >= persons - 1 && room.getPersons() <= persons + 1) {
                     if(city == room.getCityName())
                         if(hotel == room.getHotelName())
-                            if(price == room.getPrice())
-                rooms1 = rooms;
+                            if(price == room.getPrice()) {
+                        array = addRoom(array, room);
+                            }
+               // rooms1 = rooms;
             }
         }
         return rooms;
     }
+
+    private Room[] addRoom(Room[] rooms, Room room) {
+        for(int a = 0; a < rooms.length; a++) {
+                if(rooms[a] == null) {
+                    rooms[a] = room;
+                    return rooms;
+                }
+
+        }
+        Room[] array = new Room[rooms.length + 1];
+        for(int b = 0; b < rooms.length; b++) {
+            array[b] = rooms[b];
+        }
+        array[rooms.length] = room;
+
+        return array;
+    }
+
 
     @Override
     public Room[] getAll() {
