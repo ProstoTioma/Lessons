f22b21bc4c381388add777b90cfe343470bab22c
JAVA <artemyermolenko14@gmail.com>
2018-07-10 13:06:02 +0200

    Java

--- src/Homework/lesson11/Controller.java
+++ src/Homework/lesson11/Controller.java
@@ -2,15 +2,27 @@ package Homework.lesson11;
 
 public class Controller {
     private API[] apis;
+    private Room[] rooms;
 
     public Controller(API[] apis) {
         this.apis = apis;
     }
 
     public Room[] requestRooms(int price, int persons, String city, String hotel) {
-        for(API api : apis) {
+        int count = 0;
+        for(Room room : rooms) {
+            if(room.getPersons() == persons && room.getPrice() == price && room.getHotelName() == hotel && room.getCityName() == city) {
+                count++;
+            }
         }
-        return null;
+        Room[] res = new Room[count];
+        int index = 0;
+        for(Room room : rooms) {
+            if(room.getPersons() == persons && room.getPrice() == price && room.getHotelName() == hotel && room.getCityName() == city) {
+                res[index] = room;
+            }
+        }
+        return res;
     }
     public Room[] check(API api1, API api2) {
         return null;
--- src/Homework/lesson11/Demo.java
+++ src/Homework/lesson11/Demo.java
@@ -20,31 +20,31 @@ public class Demo {
 //            System.out.println(room.getId() + " "  + room.getPersons() + " " + room.getHotelName() + " " + room.getPrice());
 //        }
 
-        Room[] rooms = new Room[4];
-        rooms[0] = new Room(1, 100, 3, new Date(), "Lawandowe Zgórze", "Paris");
-        rooms[1] = new Room(2, 100, 4, new Date(), "Lawandowe Zgórze", "Paris");
-        rooms[2] = new Room(3, 700, 1, new Date(), "Elite Club", "NewYork");
-        rooms[3] = new Room(4, 1200, 10, new Date(), "Elite Hotel", "LasVegas");
-
-        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI(rooms);
-
-        Room[] room = tripAdvisorAPI.findRooms(100, 3, "Paris", "Lawandowe Zgórze");
-        for (Room room1 : room) {
-            System.out.println(room1.getId() + " " + room1.getPrice() + " " + room1.getPersons());
-        }
-
 //        Room[] rooms = new Room[4];
 //        rooms[0] = new Room(1, 100, 3, new Date(), "Lawandowe Zgórze", "Paris");
 //        rooms[1] = new Room(2, 100, 4, new Date(), "Lawandowe Zgórze", "Paris");
 //        rooms[2] = new Room(3, 700, 1, new Date(), "Elite Club", "NewYork");
 //        rooms[3] = new Room(4, 1200, 10, new Date(), "Elite Hotel", "LasVegas");
 //
-//        GoogleAPI googleAPI = new GoogleAPI(rooms);
+//        TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI(rooms);
 //
-//        Room[] room = googleAPI.findRooms(100, 3, "Paris", "Lawandowe Zgórze");
+//        Room[] room = tripAdvisorAPI.findRooms(100, 3, "Paris", "Lawandowe Zgórze");
 //        for (Room room1 : room) {
 //            System.out.println(room1.getId() + " " + room1.getPrice() + " " + room1.getPersons());
 //        }
+
+        Room[] rooms = new Room[4];
+        rooms[0] = new Room(1, 100, 3, new Date(), "Lawandowe Zgórze", "Paris");
+        rooms[1] = new Room(2, 100, 4, new Date(), "Lawandowe Zgórze", "Paris");
+        rooms[2] = new Room(3, 700, 1, new Date(), "Elite Club", "NewYork");
+        rooms[3] = new Room(4, 1200, 10, new Date(), "Elite Hotel", "LasVegas");
+
+        GoogleAPI googleAPI = new GoogleAPI(rooms);
+
+        Room[] room = googleAPI.findRooms(100, 3, "Paris", "Lawandowe Zgórze");
+        for (Room room1 : room) {
+            System.out.println(room1.getId() + " " + room1.getPrice() + " " + room1.getPersons());
+        }
 
     }
 }
