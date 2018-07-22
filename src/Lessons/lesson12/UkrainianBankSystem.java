5514f9719931f611ebce576608d274e1b3790949
JAVA <artemyermolenko14@gmail.com>
2018-07-22 19:01:56 +0200

    Java

--- src/Lessons/lesson12/UkrainianBankSystem.java
+++ src/Lessons/lesson12/UkrainianBankSystem.java
@@ -33,7 +33,7 @@ public class UkrainianBankSystem implements BankSystem {
             return;
 
         fromUser.setBalance(fromUser.getBalance() - amount - amount * fromUser.getBank().getCommission(amount));
-        toUser.setBalance(toUser.getBalance() + amount - amount * toUser.getBank().getCommission(amount));
+        toUser.setBalance(toUser.getBalance() + amount);
     }
 
     @Override
