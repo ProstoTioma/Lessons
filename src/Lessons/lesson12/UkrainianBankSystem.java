b1e5c01d71e034e6f294dba3b99888019de32acc
JAVA <artemyermolenko14@gmail.com>
2018-07-22 19:11:09 +0200

    Java

--- src/Lessons/lesson12/UkrainianBankSystem.java
+++ src/Lessons/lesson12/UkrainianBankSystem.java
@@ -63,7 +63,7 @@ public class UkrainianBankSystem implements BankSystem {
 
 
     private boolean checkFund(User user, int amount) {
-        return checkFundLimits(user, amount, user.getBank().getLimitOfFunding());
+        return checkFundLimits(user, amount, user.getBank().getLimitOfFunding()) && checkFundLimits(user, amount, user.getBalance());
     }
 
     private boolean checkFundLimits(User user, int amount, double limit) {
