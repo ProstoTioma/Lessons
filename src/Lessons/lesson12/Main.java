3d412157f7bede8e72f75ef02f7b9dc0786b15f7
JAVA <artemyermolenko14@gmail.com>
2018-07-27 16:42:19 +0200

    Java

--- src/Lessons/lesson12/Main.java
+++ src/Lessons/lesson12/Main.java
@@ -2,59 +2,94 @@ package Lessons.lesson12;
 
 public class Main {
     public static void main(String[] args) {
-        Bank usBank = new USBank(1222, "Sweden", Currency.EUR, 100, 1400, 4, 444343434);
-        Bank euBank = new EUBank(145, "Ukraine", Currency.USD, 76, 230, 5, 4872213);
-        Bank chinaBank = new ChinaBank(1, "China", Currency.EUR, 4574, 150, 2, 487221);
-
-        User user = new User(1001, "Denis", 12200, 40, "GMD", 1500, usBank);
-        User user1 = new User(1323, "Artem", 777333, 120, "ING", 13000, usBank);
-
-        User user2 = new User(138, "Oleg", 2400, 3, "UKM", 290, euBank);
-        User user3 = new User(1338, "John", 140000, 1, "ORS", 4000, euBank);
+        //UkrainianBankSystem test
+        System.out.println("UkrainianBankSystem test");
+        System.out.println();
 
 
-        User user4 = new User(476, "Ki", 3800, 94, "AWL", 390, chinaBank);
-        User user5 = new User(9674, "Ron", 678000, 4, "BOP", 9300, chinaBank);
+        //withdraw
+        System.out.println("withdraw");
+        System.out.println();
+
+        //снять деньги
+        //снять больше лимита
+        //если нету денег на счету
 
 
         BankSystem bankSystem = new UkrainianBankSystem();
 
-        bankSystem.fund(user, 600);
+
+        Bank usBank = new USBank(1222, "Sweden", Currency.EUR, 100, 1400, 4, 444343434);
+        Bank euBank = new EUBank(145, "Ukraine", Currency.USD, 76, 230, 5, 4872213);
+        Bank chinaBank = new ChinaBank(1, "China", Currency.EUR, 4574, 150, 2, 487221);
+
+
+        User user = new User(1001, "Denis", 200, 40, "GMD", 1500, usBank);
+
+
         bankSystem.withdraw(user, 150);
-        bankSystem.paySalary(user);
-        bankSystem.transferMoney(user, user1, 140);
-
-        bankSystem.fund(user1, 894);
-        bankSystem.withdraw(user1, 413);
-        bankSystem.paySalary(user1);
-        bankSystem.transferMoney(user1, user2, 150);
-
-        bankSystem.fund(user2, 590);
-        bankSystem.withdraw(user2, 30);
-        bankSystem.paySalary(user2);
-        bankSystem.transferMoney(user2, user3, 160);
-
-        bankSystem.fund(user3, 493);
-        bankSystem.withdraw(user3, 300);
-        bankSystem.paySalary(user3);
-        bankSystem.transferMoney(user3, user4, 170);
-
-        bankSystem.fund(user4, 23);
-        bankSystem.withdraw(user4, 24);
-        bankSystem.paySalary(user4);
-        bankSystem.transferMoney(user4, user5, 18);
-
-        bankSystem.fund(user5, 145);
-        bankSystem.withdraw(user5, 30);
-        bankSystem.paySalary(user5);
-        bankSystem.transferMoney(user5, user1, 10);
-
 
         System.out.println(user.toString());
+
+        bankSystem.withdraw(user, 3574);
+
+        bankSystem.withdraw(user, 460);
+
+        //fund
+        System.out.println();
+        System.out.println("fund");
+        System.out.println();
+
+
+        //пополнить счет
+        //пополнить больше лимита
+
+
+        User user1 = new User(1323, "Artem", 777333, 120, "ING", 13000, euBank);
+
+        bankSystem.fund(user1, 80);
+
         System.out.println(user1.toString());
+
+        bankSystem.fund(user1, 50000);
+
+        //transferMoney
+        System.out.println();
+        System.out.println("transferMoney");
+        System.out.println();
+
+
+        //переводем деньги
+        //превышин лимит ввода
+        //когда нету денег на счету у userFrom
+
+
+        User user2 = new User(138, "Oleg", 90, 3, "UKM", 20000, chinaBank);
+
+        bankSystem.transferMoney(user2, user, 30);
         System.out.println(user2.toString());
-        System.out.println(user3.toString());
-        System.out.println(user4.toString());
-        System.out.println(user5.toString());
+        System.out.println(user.toString());
+
+        System.out.println();
+
+        bankSystem.transferMoney(user2, user, 80);
+
+
+        //paySalary
+        System.out.println();
+        System.out.println("paySalary");
+        System.out.println();
+
+
+        //дать зарплату
+        //если зарплата больше лимита
+
+
+        User user3 = new User(8923, "James", 19000, 14, "WOG", 3000, euBank);
+
+        bankSystem.paySalary(user3);
+        System.out.println(user3);
+
+        bankSystem.paySalary(user2);
     }
 }
