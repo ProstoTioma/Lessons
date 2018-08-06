794aff062d8c8cc3631fdc4325daaf1eb7d4eb38
JAVA <artemyermolenko14@gmail.com>
2018-08-06 15:41:04 +0200

    Java

--- src/Homework/lesson15/USER/UserRepository.java
+++ src/Homework/lesson15/USER/UserRepository.java
@@ -46,15 +46,15 @@ public class UserRepository {
             return null;
         }
 
-        User curUser = findUserById(user.getId());
-        if (curUser == null)
-            return null;
+//        User curUser = findUserById(user.getId());
+//        if (curUser == null)
+//            return null;
 
-        long curUserId = curUser.getId();
+//        long curUserId = curUser.getId();
 
         int index = 0;
         for (User us : users) {
-            if (us != null && us.getId() == curUserId) {
+            if (us != null) {
                 users[index] = user;
                 break;
             }
